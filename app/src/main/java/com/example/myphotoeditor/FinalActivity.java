c4ffaad54370041260b0b5eb23b5ac56140a77d2
Kashish Kataria <kashishkataria8660@gmail.com>
2023-03-22 16:10:36 +0530

    realised startActivityResult() is deprecated and thus causing issue

--- app/src/main/java/com/example/myphotoeditor/FinalActivity.java
+++ app/src/main/java/com/example/myphotoeditor/FinalActivity.java
@@ -18,22 +18,9 @@ ActivityFinalBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityFinalBinding.inflate(getLayoutInflater());
+        binding = ActivityFinalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
-        dsPhotoEditorIntent.setData(imgUri);
-        dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "My Editor");
-//                int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
-//                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
-        startActivityForResult(dsPhotoEditorIntent, 200); //used to start a new activity and receive a result back from it. When you call this method, you need to pass in an integer request code that identifies the request you're making to the new activity. In this case it's Uri path.
-    }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case 200: //We passed 200 above
+        binding.imageView3.setImageURI(getIntent().getData());
 
-            }
-        }
     }
 }
--- app/src/main/java/com/example/myphotoeditor/MainActivity.java
+++ app/src/main/java/com/example/myphotoeditor/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -27,26 +28,33 @@ public class MainActivity extends AppCompatActivity {
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ImagePicker.Companion.with(MainActivity.this).crop().start(); //from Image picker Library of Dhaval(Used to pick image from Gallery/Camera and crop it)
-//                Intent intent = new Intent();
-//                intent.setAction(Intent.ACTION_GET_CONTENT);
-//                intent.setType("image/*");
-//                startActivityForResult(intent, 40); //We are passing custom requestcode here, you can pass here 50 too and then put check in onActivityResult if we really got 50 or not
+//                ImagePicker.Companion.with(MainActivity.this).crop().start(); //from Image picker Library of Dhaval(Used to pick image from Gallery/Camera and crop it)
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 40); //We are passing custom requestcode here, you can pass here 50 too and then put check in onActivityResult if we really got 50 or not
             }
         });
     }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) { //from Image picker Library of Dhaval, it'll run as soon as we pick image form gallery or camera
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { //from Image picker Library of Dhaval, it'll run as soon as we pick image form gallery or camera(we have called startActivityForResult above)
         super.onActivityResult(requestCode, resultCode, data);
-//        if (requestCode==40){
+        if (requestCode==40){
             if (data.getData()!=null){
                 Intent dsPhotoEditorIntent = new Intent(this, DsPhotoEditorActivity.class);
-
                 dsPhotoEditorIntent.setData(data.getData());
                 dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_OUTPUT_DIRECTORY, "My Editor");
-//                int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
-//                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+                int[] toolsToHide = {DsPhotoEditorActivity.TOOL_ORIENTATION, DsPhotoEditorActivity.TOOL_CROP};
+                dsPhotoEditorIntent.putExtra(DsPhotoEditorConstants.DS_PHOTO_EDITOR_TOOLS_TO_HIDE, toolsToHide);
+                Log.d("toast",data.getData().toString() );
                 startActivityForResult(dsPhotoEditorIntent, 200);
             }
-//        }
+        }
+        if (requestCode==200){
+            Intent intent = new Intent(this, FinalActivity.class);
+            intent.setData(data.getData());
+            Log.d("toast123", data.getData().toString() );
+//            Toast.makeText(this, data.getData().toString(), Toast.LENGTH_LONG).show();
+            startActivity(intent);
+        }
     }
 }
